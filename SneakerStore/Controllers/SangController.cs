c502d88248d8acfdb1ddbda6e2c7f5b24d7724b4
tansang1230 <sangnguyen01678@gmail.com>
2023-09-23 13:53:32 +0700

    Controller

---
+++ SneakerStore/Controllers/SangController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SneakerStore.Controllers
+{
+    public class SangController : Controller
+    {
+        // GET: Sang
+    }
+}
