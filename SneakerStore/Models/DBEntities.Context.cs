8e9763a5dd6e107e4994405307162e86c025ec27
tansang1230 <sangnguyen01678@gmail.com>
2023-10-27 18:08:38 +0700

    Update Layout

--- SneakerStore/Models/DBEntities.Context.cs
+++ SneakerStore/Models/DBEntities.Context.cs
@@ -25,11 +25,5 @@ namespace SneakerStore.Models
             throw new UnintentionalCodeFirstException();
         }
     
-        public virtual DbSet<AdminUser> AdminUsers { get; set; }
-        public virtual DbSet<Category> Categories { get; set; }
-        public virtual DbSet<Customer> Customers { get; set; }
-        public virtual DbSet<OrderDetail> OrderDetails { get; set; }
-        public virtual DbSet<OrderPro> OrderProes { get; set; }
-        public virtual DbSet<Product> Products { get; set; }
     }
 }
